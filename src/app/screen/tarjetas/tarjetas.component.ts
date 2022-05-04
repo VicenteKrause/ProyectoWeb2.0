d6034808e819c235a47d21c75de1bca0bdb3ce25
LeomardoCortez <leonardo.cortez.acevedo@gmail.com>
2022-05-04 17:25:20 -0400

    Cambio varios sobre angular

--- src/app/screen/tarjetas/tarjetas.component.ts
+++ src/app/screen/tarjetas/tarjetas.component.ts
@@ -6,10 +6,36 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./tarjetas.component.scss']
 })
 export class TarjetasComponent implements OnInit {
+  
+  exampleNum = 150;
 
-  constructor() { }
+  constructor() {
 
+   }
+  // fetch(){
+  //   let get = Document.getElementById("customRange1").value;
+  //   Document.getElementById("put").value = get;
+  // }
   ngOnInit(): void {
   }
 
+  suma(){
+    if(this.exampleNum<10000){
+      this.exampleNum = this.exampleNum+50;
+    }else{
+      window.alert("No puede exceder los $10.000 pesitos");
+    }
+   
+  }
+  resta(){
+    if(this.exampleNum>150){
+      this.exampleNum = this.exampleNum-50;
+    }else{
+      window.alert("No puede ser inferior a $150 pesitos");
+    }
+  }
+  home(){
+    window.location.href="/home";
+    window.alert("Recarga exitosa");
+  }
 }
